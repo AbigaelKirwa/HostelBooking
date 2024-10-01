41f21cf11e1de34baf641d879f97f3220564577e
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-10-01 06:51:19 +0300

    feat(register):created action.tsx and page.tsx

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Register(){
+
+    return(
+        <div>
+            <div className="nav_bar">
+                
+            </div>
+        </div>
+    )
+}
