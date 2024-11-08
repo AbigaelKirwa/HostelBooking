dd4a8a51843c2d2bde2b077a6c8abdcffc59fbc3
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-11-08 21:42:27 +0300

    feat(success and cancel pages):adds success and cancel pages after payment fully responsive

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,16 +1,22 @@
 "use client";
+import ButtonPink from "@/components/Button";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import Link from "next/link";
 
 export default function Cancel() {
     return (
+        <>
+        <Navbar/>
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "2rem" }}>
-            <h1>Payment Canceled</h1>
-            <p>Your payment was not completed. You can try again if you wish.</p>
-            <Link href="/">
-                <a style={{ color: "#0070f3", textDecoration: "underline", marginTop: "1rem" }}>
-                    Return to Home
-                </a>
+            <h1 className="text-2xl font-semibold">Payment Canceled</h1>
+            <p className="text-sm mt-5 text-slate-800">Your payment was not completed. You can try again if you wish.</p>
+            <Link href="/accomodations" className="mt-5">
+                <ButtonPink paddingX="3em" paddingY="1.8em" >Return to Accomodations</ButtonPink>
             </Link>
+            <img src="/images/cancel.svg" alt="win" className="w-1/3" />
         </div>
+        <Footer/>
+        </>
     );
 }
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ButtonPink from "@/components/Button";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
@@ -7,15 +8,15 @@ export default function Success() {
     return (
         <>
             <Navbar/>
-            <div style={{ height:'40vh', display: "flex", flexDirection: "column", alignItems: "center", marginTop: "2rem" }}>
-                <h1>Payment Successful!</h1>
-                <p>Thank you for your paying. Your payment was processed successfully.</p>
-                <Link href="/">
-                    <p style={{ color: "#0070f3", textDecoration: "underline", marginTop: "1rem" }}>
-                        Return to Home
-                    </p>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "2rem" }}>
+                <h1 className="text-2xl font-semibold">Payment Successful!</h1>
+                <p className="text-sm mt-5 text-slate-800">Thank you for your paying. Your payment was processed successfully.</p>
+                <Link href="/" className="mt-5">
+                    <ButtonPink paddingX="3em" paddingY="1.8em" >Return to Home</ButtonPink>
                 </Link>
+                <img src="/images/success.svg" alt="win" className="w-1/3" />
             </div>
+            <Footer/>
         </>
     );
 }
